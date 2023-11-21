259776b878faf355391de59640ef59b2daeab6c5
Akul0105 <bhouvanesh@gmail.com>
2023-11-21 13:43:34 +0400

    before

---
+++ src/MemeForm.js
@@ -0,0 +1,2 @@
+//This component would handle the form that is used to create a meme. It would receive the selected template, top text, and bottom text as props, and would handle the form submission
+
